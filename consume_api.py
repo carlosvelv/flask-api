1ec17576047d3f6587009c00f2edbbca0e538749
Carlos Velazquez <a215210531@unison.mx>
2022-06-27 18:07:56 -0700

    actualice codigo

--- consume_api.py
+++ consume_api.py
@@ -3,19 +3,21 @@ import requests
 
 BASE = 'http://127.0.0.1:5001/'
 
-print('primero agregaremos varios libros')
 
-# data = [{'nombre' : 'Violeta', 'autor' : 'Isabel Allende','ventas' : 5153},
-#         {'nombre' : 'La insoportable levedad del ser', 'autor' : 'Milan Kundera','ventas' : 5100},
-#         {'nombre' : 'Al paraíso', 'autor' : 'Hanya Yanagihara','ventas' : 5000},]
+#Agregando varios libros
+data = [{'nombre' : 'Violeta', 'autor' : 'Isabel Allende','ventas' : 5153},
+        {'nombre' : 'La insoportable levedad del ser', 'autor' : 'Milan Kundera','ventas' : 5100},
+        {'nombre' : 'Al paraíso', 'autor' : 'Hanya Yanagihara','ventas' : 5000},]
 
-# for i in range(len(data)):
-#     response = requests.put(BASE + 'libro/' + str(i), data=data[i])
-#     print(response)
+for i in range(len(data)):
+    response = requests.put(BASE + 'libro/' + str(i), data=data[i])
+    print(response)
 
-# response = requests.get(BASE + 'video/1')
-# print(response.json())
+#Solicitando información de un libro
+response = requests.get(BASE + 'video/1')
+print(response.json())
 
+#Eliminado registro de la tabla
 response = requests.delete(BASE + 'libro/1')
 print(response)
 
